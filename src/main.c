e850d4c8e513e8f4732dac1dc87f24c323d42ab6
JekiTheMonkey <tonysidenko@gmail.com>
2022-05-14 12:06:05 +0200

    Switch from tabs to spaces

--- src/main.c
+++ src/main.c
@@ -11,91 +11,91 @@
 #include <unistd.h>
 
 /*
-	TOIMPLEMENT
-	Left/Right key arrows to modify the written command in any position.
-	Top/Down key arrows to leaf through command history.
-	Tab to get suggestions.
+    TOIMPLEMENT
+    Left/Right key arrows to modify the written command in any position.
+    Top/Down key arrows to leaf through command history.
+    Tab to get suggestions.
 */
 
 #define HANDLE_ERROR(code, str, head) \
-	if (code == MT_ERR) \
-	{ \
-		mt_perror(NULL); \
-		st_token_clear(head); \
-		continue; \
-	}
+    if (code == MT_ERR) \
+    { \
+        mt_perror(NULL); \
+        st_token_clear(head); \
+        continue; \
+    }
 
 #define FINISH() do { cont = 0; break; } while (0)
 
 void chld_hdl(int s)
 {
-	signal(SIGCHLD, &chld_hdl);
-	TRACEE("Signal %d has arrived\n", s);
-	check_zombie();
-	TRACEL("\n");
-	UNUSED(s);
+    signal(SIGCHLD, &chld_hdl);
+    TRACEE("Signal %d has arrived\n", s);
+    check_zombie();
+    TRACEL("\n");
+    UNUSED(s);
 }
 
 int main()
 {
-	int res, cont= 1;
-	size_t i;
-	char buf[4096], *str;
-	st_invite_msg *invite_msg = NULL;
-	st_token *tokens = NULL;
-	st_command *cmds, *cmd;
+    int res, cont= 1;
+    size_t i;
+    char buf[4096], *str;
+    st_invite_msg *invite_msg = NULL;
+    st_token *tokens = NULL;
+    st_command *cmds, *cmd;
 
-	signal(SIGCHLD, &chld_hdl);
-	st_command_init();
-	clear_post_execution_msg();
-	while (cont)
-	{
-		st_invite_msg_form(&invite_msg);
-		st_invite_msg_print(invite_msg);
-		str = read_string(buf, sizeof(buf));
-		tokens = form_token_list(str);
-		if (tokens)
-		{
-			remove_quotes(&tokens);
-			unify_multiple_tokens(tokens);
+    signal(SIGCHLD, &chld_hdl);
+    st_command_init();
+    clear_post_execution_msg();
+    while (cont)
+    {
+        st_invite_msg_form(&invite_msg);
+        st_invite_msg_print(invite_msg);
+        str = read_string(buf, sizeof(buf));
+        tokens = form_token_list(str);
+        if (tokens)
+        {
+            remove_quotes(&tokens);
+            unify_multiple_tokens(tokens);
 #ifdef _TRACE
-			printf("Tokens - {\n");
-			st_token_print(tokens, TAB);
-			printf("}\n");
+            printf("Tokens - {\n");
+            st_token_print(tokens, TAB);
+            printf("}\n");
 #endif
-			analyze_token_types(tokens);
-			res = check_syntax(tokens);
-			HANDLE_ERROR(res, NULL, &tokens);
+            analyze_token_types(tokens);
+            res = check_syntax(tokens);
+            HANDLE_ERROR(res, NULL, &tokens);
 
-			cmds = st_commands_create(tokens);
+            cmds = st_commands_create(tokens);
 #ifdef _TRACE
-			for (i = 0ul, cmd = cmds; cmd; i++, cmd = cmd->next)
-			{
-				printf("Command %lu - {\n", i + 1);
-				st_command_print(cmd, TAB);
-				printf("}\n");
-			}
+            for (i = 0ul, cmd = cmds; cmd; i++, cmd = cmd->next)
+            {
+                printf("Command %lu - {\n", i + 1);
+                st_command_print(cmd, TAB);
+                printf("}\n");
+            }
 #endif
-			st_command_pass_ownership(cmds);
-			cmds = NULL;
-			if (execute_commands() == -1)
-				FINISH();
+            st_command_pass_ownership(cmds);
+            cmds = NULL;
+            if (execute_commands() == -1)
+                FINISH();
 
-			st_token_clear(&tokens);
-		}
+            st_token_clear(&tokens);
+        }
 
-		res = printf("%s", post_execution_msg);
-		clear_post_execution_msg();
-		if (res)
-			putchar(10);
-	}
-	printf("exit\n");
-	st_invite_msg_delete(&invite_msg);
-	st_commands_free();
+        res = printf("%s", post_execution_msg);
+        clear_post_execution_msg();
+        if (res)
+            putchar(10);
+    }
+    printf("exit\n");
+    st_invite_msg_delete(&invite_msg);
+    st_commands_free();
 
-	/* avoid warning on release build */
-	UNUSED(cmd);
-	UNUSED(i);
-	return 0;
+    /* avoid warning on release build */
+    UNUSED(cmd);
+    UNUSED(i);
+    return 0;
 }
 
--- src/st_redirector.c
+++ src/st_redirector.c
@@ -8,40 +8,40 @@
 
 st_redirector *st_redirector_create_empty()
 {
-	st_redirector *item = (st_redirector *) malloc(sizeof(st_redirector));
-	ATRACEE(item);
-	item->fd = -1;
-	item->app = 0;
-	item->path = NULL;
-	item->dir = rd_ukw;
-	TRACEL("\n");
-	return item;
+    st_redirector *item = (st_redirector *) malloc(sizeof(st_redirector));
+    ATRACEE(item);
+    item->fd = -1;
+    item->app = 0;
+    item->path = NULL;
+    item->dir = rd_ukw;
+    TRACEL("\n");
+    return item;
 }
 
 void st_redirector_delete(st_redirector **item)
 {
-	LOGFNPP(item);
-	FREE_IFEX((*item)->path);
-	FREE(*item);
-	TRACEL("\n");
+    LOGFNPP(item);
+    FREE_IFEX((*item)->path);
+    FREE(*item);
+    TRACEL("\n");
 }
 
 void st_redirector_print(st_redirector *item)
 {
-	printf("path '%s' - '%p', fd '%d', app '%d', dir '%s'", item->path,
-		(void *) item, item->fd, item->app, item->dir == rd_in ? "in" :
-		item->dir == rd_out ? "out" : "unknown");
+    printf("path '%s' - '%p', fd '%d', app '%d', dir '%s'", item->path,
+        (void *) item, item->fd, item->app, item->dir == rd_in ? "in" :
+        item->dir == rd_out ? "out" : "unknown");
 }
 
 st_redirector *st_redirector_dup(const st_redirector *src)
 {
-	TRACEE("Source addr '%p'\n", (void *) src);
-	assert(src);
-	st_redirector *dest = (st_redirector *) malloc (sizeof(st_redirector));
-	ATRACE(dest);
-	memcpy(dest, src, sizeof(st_redirector));
-	if (src->path)
-		dest->path = strdup(src->path);
-	TRACEL("\n");
-	return dest;
+    TRACEE("Source addr '%p'\n", (void *) src);
+    assert(src);
+    st_redirector *dest = (st_redirector *) malloc (sizeof(st_redirector));
+    ATRACE(dest);
+    memcpy(dest, src, sizeof(st_redirector));
+    if (src->path)
+        dest->path = strdup(src->path);
+    TRACEL("\n");
+    return dest;
 }
